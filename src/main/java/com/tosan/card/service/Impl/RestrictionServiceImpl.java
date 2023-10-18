0b6074963b96281e8f5d56d00451a92e94113532
Pedram <pedadashii@gmail.com>
2023-10-18 13:02:23 +0330

    refactor : [ rename and move related methods between services ]

--- src/main/java/com/tosan/card/service/Impl/RestrictionServiceImpl.java
+++ src/main/java/com/tosan/card/service/Impl/RestrictionServiceImpl.java
@@ -1,14 +1,15 @@
 package com.tosan.card.service.Impl;
 
 
-
 import com.tosan.card.base.service.BaseServiceImpl;
 import com.tosan.card.entity.Restriction;
+import com.tosan.card.entity.enumuration.Period;
 import com.tosan.card.repository.RestrictionRepository;
 import com.tosan.card.service.RestrictionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -24,5 +25,29 @@ public class RestrictionServiceImpl extends BaseServiceImpl<Restriction, Long, R
     @Override
     public Optional<Restriction> findByName(String name) {
         return repository.findByName(name);
+    }
+
+    @Override
+    public void setPeriodDaysFromPeriodType(Restriction restriction) {
+        if (!restriction.getPeriod().name().equals(Period.CUSTOM.name())) {
+            switch (restriction.getPeriod()) {
+                case DAILY -> restriction.setPeriodDays(1);
+                case WEEKLY -> restriction.setPeriodDays(7);
+                case MONTHLY -> restriction.setPeriodDays(30);
+            }
+        }
+    }
+
+    @Override
+    public void setNumberOfDaysLeftRestriction(Restriction restriction) {
+        restriction.setNumberOfDaysLeftRestriction(
+                restriction.getPeriodDays() *
+                restriction.getPeriodRepeat());
+
+    }
+
+    @Override
+    public void setPeriodStartDate(Restriction restriction) {
+        restriction.setPeriodStartDate(LocalDate.now());
     }
 }
