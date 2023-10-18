47a3ce07981ca578e2931ce1f56d246da0214d1f
Pedram <pedadashii@gmail.com>
2023-10-18 15:18:46 +0330

    feat : [ add documentation with swagger for client controller ]

--- src/main/java/com/tosan/card/controller/ClientController.java
+++ src/main/java/com/tosan/card/controller/ClientController.java
@@ -6,6 +6,11 @@ import com.tosan.card.dto.response.BankAccountResponseDTO;
 import com.tosan.card.dto.response.RestrictionResponseDTO;
 import com.tosan.card.entity.Users;
 import com.tosan.card.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,6 +29,11 @@ public class ClientController {
 
     private final ClientService clientService;
 
+    @Operation(summary = "change Account Password with DTO",
+            description = "client must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid new password")})
     @PutMapping("/change-account-password")
     public void changeAccountPassword(
             @Valid @RequestBody ChangeAccountPasswordDTO changeAccountPasswordDTO,
@@ -32,6 +42,11 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "add Interest Free Bank Account with DTO",
+            description = "client must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid fields in DTO")})
     @PostMapping("/add-interest-free-bank-account")
     public void addInterestFreeBankAccount(
             @Valid @RequestBody BankAccountRequestDTO bankAccountDTO,
@@ -40,6 +55,11 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "show Bank Account with bank account number",
+            description = "desired bank account must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid digits bank account number")})
     @GetMapping("/show-bank-account/{bankAccountNumber}")
     public BankAccountResponseDTO showBankAccount(
             @PathVariable @Valid @Min(100000000L) @Max(999999999999L) Long bankAccountNumber,
@@ -48,11 +68,20 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "show all Bank Account ",
+            description = "at least one bank account must be exist")
+    @ApiResponses(value =
+    @ApiResponse(responseCode = "200", description = "Ok"))
     @GetMapping("/show-all-bank-accounts")
     public List<BankAccountResponseDTO> showAllBankAccount(Authentication authentication) {
         return clientService.showAllBankAccounts(((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "add Periodic Restriction with DTO",
+            description = "at least one bank account must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid fields in DTO")})
     @PostMapping("/add-periodic-restriction")
     public void addPeriodicRestriction(
             @Valid @RequestBody PeriodicRestrictionRequestDTO periodicRestrictionRequestDTO,
@@ -61,6 +90,11 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "show Restriction with restriction name",
+            description = "desired restriction must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid restriction name")})
     @GetMapping("/show-restriction/{restrictionName}")
     public RestrictionResponseDTO showRestriction(
             @PathVariable @Valid @NotBlank String restrictionName,
@@ -69,11 +103,20 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "show all Restriction",
+            description = "at least one restriction must be exist")
+    @ApiResponses(value =
+    @ApiResponse(responseCode = "200", description = "Ok"))
     @GetMapping("/show-all-restrictions")
     public List<RestrictionResponseDTO> showAllRestrictions(Authentication authentication) {
         return clientService.showAllRestrictions(((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "add Credit Card (with periodic restriction) with DTO",
+            description = "at least one restriction must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid fields in DTO")})
     @PostMapping("/add-credit-card-with-restriction")
     public void addCreditCardWithRestriction(
             @Valid @RequestBody BankCardRequestDTO bankCardRequestDTO,
@@ -82,6 +125,13 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "change Card Passcode with DTO",
+            description = "if credit card is new must be change passcode because now that credit card "+
+                          "is inactive else at least one credit card must be exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid digits bank credit card number OR digits new passcode")})
     @PutMapping("/change-card-passcode")
     public void changeCardPasscode(
             @Valid @RequestBody ChangeCardPasscodeDTO changeCardPasscodeDTO, Authentication authentication) {
@@ -89,6 +139,13 @@ public class ClientController {
                 ((Users) authentication.getPrincipal()).getId());
     }
 
+    @Operation(summary = "reset Card Passcode",
+            description = "if the passcode of this credit card is forgotten,"+
+    "it can be returned to the default passcode, considering that a new passcode has already"+
+    "been chosen for this card.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid digits bank credit card number")})
     @PutMapping("/reset-card-passcode/{cardNumber}")
     public void resetCardPasscode(
             @PathVariable @Valid @Pattern(regexp = "^[1-9][0-9]{15}$") String cardNumber
