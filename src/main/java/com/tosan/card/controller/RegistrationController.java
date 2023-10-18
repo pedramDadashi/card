8793629276c944c73fdb4882a6be498e54319f5f
Pedram <pedadashii@gmail.com>
2023-10-18 15:33:00 +0330

    feat : [ add documentation with swagger for registration controller ]

--- src/main/java/com/tosan/card/controller/RegistrationController.java
+++ src/main/java/com/tosan/card/controller/RegistrationController.java
@@ -3,6 +3,9 @@ package com.tosan.card.controller;
 
 import com.tosan.card.dto.request.RegularClientRegistrationDTO;
 import com.tosan.card.service.RegistrationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,14 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
 
+    @Operation(summary = "regular Client SingUp with DTO",
+            description = "you can create a regular account for yourself for use the services in app. " +
+                          "email and password have strong validity." +
+                          "the input password is stored encoded from in the database." +
+                          "access to this service is free.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Invalid fields in DTO")})
     @PostMapping("/signup-regular-client")
     public void regularClientSingUp(
             @Valid @RequestBody RegularClientRegistrationDTO regularClientRegistrationDTO) {
